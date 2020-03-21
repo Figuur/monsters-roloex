d8f4dbb50eb97ad543e1875dcbb7e87ef0d20664
Figuur <lmulder_89@hotmail.com>
2020-03-21 22:41:39 +0100

    Updates

---
+++ precache-manifest.ebd18b30e115ff6d89b7eaf9a331e290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed18e8e77bb57fbb4e91b22ed4abf2f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a7d0a1c82572080ef2a3",
+    "url": "/monsters-rolodex/static/css/main.fb15ae77.chunk.css"
+  },
+  {
+    "revision": "640fa3f2e5b37ee859bf",
+    "url": "/monsters-rolodex/static/js/2.c2a893ac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.c2a893ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d0a1c82572080ef2a3",
+    "url": "/monsters-rolodex/static/js/main.d88c9a00.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  }
+]);
